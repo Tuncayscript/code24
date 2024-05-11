f6d4257e7803f983787a729270250e3e168e26ce
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-05-11 21:14:45 +0400

    Update parser.cpp

--- antlr-llvm/syntax/parser.cpp
+++ antlr-llvm/syntax/parser.cpp
@@ -1,4 +1,4 @@
-#include <antlr-llvm/syntax/parser.hpp> // Include ANTLR generated parser header
+#include <antlr-llvm/syntax/CodeParser.g4.cpp> // Include ANTLR generated parser header
 
 // Function to parse VBA source code and return the parse tree
 ParseTree* parseCode(const std::string& source) {
