5416503176a0f4313c7ec9512e89e54e481628fa
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-05-19 00:05:32 +0400

    Create AppTest.java

---
+++ packages/test/java/itgss/com/AppTest.java
@@ -0,0 +1,12 @@
+package itgss.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
