56ff2082c46103a4d5613ce6e27d76d3095d8d26
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-05-19 15:00:27 +0400

    Create Additions.h

---
+++ runtime/Foundation/Base/Additions.h
@@ -0,0 +1,37 @@
+#ifndef __Additions_h__
+#define __Additions_h__
+
+#import	<Base/GSVersionMacros.h>
+#import	<Base/GNUstep.h>
+
+
+#if !(defined(NeXT_RUNTIME) || defined(Apple_RUNTIME))
+#import	<Base/GSBlocks.h>
+#endif
+#import	<Base/GSFunctions.h>
+#import	<Base/GSLocale.h>
+#import	<Base/GSLock.h>
+#import	<Base/GSMime.h>
+#import	<Base/GSXML.h>
+#import	<Base/Unicode.h>
+
+#import	<Base/NSArray+Base.h>
+#import	<Base/NSAttributedString+Base.h>
+#import	<Base/NSBundle+Base.h>
+#import	<Base/NSCalendarDate+Base.h>
+#import	<Base/NSData+Base.h>
+#import	<Base/NSDebug+Base.h>
+#import	<Base/NSFileHandle+Base.h>
+#import	<Base/NSLock+Base.h>
+#import	<Base/NSMutableString+Base.h>
+#import	<Base/NSNetServices+Base.h>
+#import	<Base/NSNumber+Base.h>
+#import	<Base/NSObject+Base.h>
+#import	<Base/NSProcessInfo+Base.h>
+#import	<Base/NSStream+Base.h>
+#import	<Base/NSString+Base.h>
+#import	<Base/NSTask+Base.h>
+#import	<Base/NSThread+Base.h>
+#import	<Base/NSURL+Base.h>
+
+#endif /* __Additions_h_GNUSTEP_BASE_INCLUDE */
