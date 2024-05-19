df89677a99193e8b301def49048560a75fca7292
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-05-19 17:02:01 +0400

    Update and rename Compiler.h to Hub.h

--- include/lang/Basic/Hub.h
+++ include/lang/Basic/Hub.h
@@ -1,17 +1,5 @@
-//===--- Compiler.h - Compiler specific definitions -------------*- C++ -*-===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-
-#ifndef SWIFT_BASIC_COMPILER_H
-#define SWIFT_BASIC_COMPILER_H
+#ifndef HUB_H
+#define HUB_H
 
 #if defined(_MSC_VER) && !defined(__clang__)
 #define SWIFT_COMPILER_IS_MSVC 1
