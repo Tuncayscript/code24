52391a430bfd3d27795c15523e6ae5dec41b1ce2
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-05-23 14:27:48 +0400

    Deployment of Universal C Runtime (UCRT)
    It provides the implementation of the C99 standard library functions and goes alongside the Visual C++ Runtime (VCruntime) libraries.
    
    Here's a breakdown of UCRT in Code:
    
    What it is: UCRT offers the standard C library functions that your C++ code interacts with. It's a Code component, meaning it's part of the Code installation.
    
    Benefits:
    
    Standardization: UCRT adheres to the C99 standard, ensuring wider compatibility with C code.
    
    Simplified Deployment: Since it's part of Code, you don't need to bundle UCRT with your application for most users. The Visual C++ Redistributable package handles installation for those who lack the required UCRT version.
    
    Relation to Runtime: Runtime libraries provide compiler-specific functionalities like exception handling and debugging support. They work alongside UCRT for a complete C++ runtime environment.

---
+++ runtime/ucrt/io.h
@@ -0,0 +1,13 @@
+//
+// io.h
+//
+//      Copyright (c) ITGSS Corporation. All rights reserved.
+//
+// This file declares the low-level I/O and file handling functionality.
+//
+#pragma once
+#ifndef _INC_IO // include guard for 3rd party interop
+#define _INC_IO
+
+#include <corecrt_io.h>
+#endif // _INC_IO
