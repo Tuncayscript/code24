d0bb8fb29d52df69b0cb84b36f91f0699923e073
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-06-25 00:33:34 +0400

    Update coverage.cpp

--- src/coverage.cpp
+++ src/coverage.cpp
@@ -30,8 +30,8 @@
 #include <llvm/ADT/StringMap.h>
 #include <llvm/Support/raw_ostream.h>
 
-#include "language.h"
-#include "language_internal.h"
+#include "code.h"
+#include "code_internal.h"
 
 using namespace llvm;
 
