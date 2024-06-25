0810cfd365701c3d092678ff4974f3a960839e2c
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-06-25 12:40:37 +0400

    Update intrinsics.h

--- src/intrinsics.h
+++ src/intrinsics.h
@@ -1,18 +1,24 @@
 /*
- * Copyright (c) 2024, ITGSS Corporation. All rights reserved.
+ * Copyright (c) 2024, NeXTech Corporation. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-  *
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
  *
- * Contact with ITGSS, 651 N Broad St, Suite 201, in the
- * city of Middletown, zip code 19709, and county of New Castle, state of Delaware.
+ * Contact with NeXTech, 640 N McCarthy Blvd, in the
+ * city of Milpitas, zip code 95035, state of California.
  * or visit www.it-gss.com if you need additional information or have any
  * questions.
+ *
  */
+
+// About:
+// Author(-s): Tunjay Akbarli (tunjayakbarli@it-gss.com)
+// Date: Tuesday, June 25, 2024
+// Technology: C/C++20 - ISO/IEC 14882:2020(E)
 
 #define INTRINSICS \
     /*  wrap and unwrap */ \
