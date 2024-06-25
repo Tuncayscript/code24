0871ed4298eda8de700afd43e3238d76cb79eda6
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-06-25 12:12:57 +0400

    Update gc.c

--- src/gc.c
+++ src/gc.c
@@ -1,4 +1,24 @@
-// This file is a part of Julia. License is MIT: https://julialang.org/license
+/*
+ * Copyright (c) 2024, NeXTech Corporation. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * Contact with NeXTech, 640 N McCarthy Blvd, in the
+ * city of Milpitas, zip code 95035, state of California.
+ * or visit www.it-gss.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+// About:
+// Author(-s): Tunjay Akbarli (tunjayakbarli@it-gss.com)
+// Date: Sunday, May 19, 2024
+// Technology: C/C++20 - ISO/IEC 14882:2020(E) 
 
 #include "gc.h"
 #include "gc-page-profiler.h"
