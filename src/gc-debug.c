3abb3b481407fb74221ca2d959a69125312040f8
Tunjay Akbarli <128295620+Tuncayscript@users.noreply.github.com>
2024-06-25 11:43:56 +0400

    Update gc-debug.c

--- src/gc-debug.c
+++ src/gc-debug.c
@@ -1,4 +1,26 @@
-// This file is a part of Julia. License is MIT: https://languagelang.org/license
+/*
+ * Copyright (c) 2024, NeXTech Corporation. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * Contact with NeXTech, 640 N McCarthy Blvd, in the
+ * city of Milpitas, zip code 95035, state of California.
+ * or visit www.it-gss.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+// About:
+// Author(-s): Tunjay Akbarli (tunjayakbarli@it-gss.com)
+// Date: Wednesday, June 6, 2024
+// Technology: C/C++20 - ISO/IEC 14882:2020(E) 
+// Purpose: Components of the front-end, for obtaining and translating syntax trees
+
 
 #include "gc.h"
 #include "code.h"
